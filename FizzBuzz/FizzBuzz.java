b5769932fb1b124a7e6427bf4dd547c1f4d239b5
Georgia Asimaki <Georgia.Asimaki@solera.com>
2022-11-18 13:46:50 +0200

    FizzBuzz task 3

--- FizzBuzz/FizzBuzz.java
+++ FizzBuzz/FizzBuzz.java
@@ -1,5 +1,7 @@
 package BerlinClock.FizzBuzz;
 
+import BerlinClock.BerlinClock;
+
 import java.util.Scanner;
 
 public class FizzBuzz {
@@ -35,18 +37,61 @@ public class FizzBuzz {
         return out;
     }
 
+    public String Custom(String nums, String names, Integer number) {
+        String[] name = names.split(",");
+        String[] num = nums.split(",");
+
+        String out = "";
+
+        Integer n;
+
+        for(int i=0; i<name.length;i++) {
+            n = Integer.valueOf(num[i]);
+            if (number%n==0){
+                out = out + " " + name[i];
+            }
+        }
+
+        if (out==""){
+            out = Integer.toString(number);
+        }
+        return out;
+    }
+
 
     public static void main(String[] args){
 
         Integer number;
 
         Scanner myObj = new Scanner(System.in);
-        System.out.println("Enter Number");
+        System.out.println("Enter 1 for Custom or 2 for not custom");
         String input = myObj.nextLine();
-        number = Integer.valueOf(input);
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        //System.out.println(fizzBuzz.multipleOfThree(number) + " " + fizzBuzz.multipleOfFive(number));
-        System.out.println(fizzBuzz.multipleOf(number));
+        if (input=="2") {
+            Scanner myObjN = new Scanner(System.in);
+            System.out.println("Enter Number");
+            String inputN = myObjN.nextLine();
+            number = Integer.valueOf(inputN);
+            FizzBuzz fizzBuzz = new FizzBuzz();
+            //System.out.println(fizzBuzz.multipleOfThree(number) + " " + fizzBuzz.multipleOfFive(number));
+            System.out.println(fizzBuzz.multipleOf(number));
+        }
+        else{
+            Scanner myObjC = new Scanner(System.in);
+            System.out.println("Enter Numbers divided by commas that you want to check their multiples");
+            String inputNumbers = myObjC.nextLine();
+            System.out.println("Enter the outputs for each multiple divided by commas");
+            String inputStrings = myObjC.nextLine();
+
+            System.out.println("Enter Number");
+            String inputC = myObjC.nextLine();
+            number = Integer.valueOf(inputC);
+
+            FizzBuzz fizzBuzz = new FizzBuzz();
+
+            System.out.println(fizzBuzz.Custom(inputNumbers,inputStrings, number));
+        }
+
+
 
     }
 }
