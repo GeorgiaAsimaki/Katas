df60275522b920d900f666e8e6a93e3b96c632c7
Georgia Asimaki <Georgia.Asimaki@solera.com>
2022-11-18 18:05:38 +0200

    Bug

---
+++ BugHunt/last.java
@@ -0,0 +1,24 @@
+package BerlinClock.BugHunt;
+
+public class last {
+    @Override
+    public boolean equals(Object o){
+        ...
+        CheckpointStatistics that = (CheckpointStatistics) o;
+        return id == that.id &&
+                savepoint == that.savepoint  &&
+                triggerTimestamp == that.triggerTimestamp  &&
+                stateSize == that.stateSize  &&
+                duration == that.duration  &&
+                alignmentBuffered == that.alignmentBuffered  &&
+                processedData == processedData  &&
+                persistedData == that.persistedData  &&
+                numSubtasks == that.numSubtasks  &&
+                status == that.status  &&
+                Object.equals(checkpointType, that.checkpointType)  &&
+                Object.equals(
+                        checkpointStatisticsPerTask,
+                        that.checkpointStatisticsPerTask
+                );
+    }
+}
