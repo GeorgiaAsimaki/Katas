e9f5431e0c743e5a606bacd5a15aaa50b0e48208
Lungu Marius <Marius.Lungu@solera.com>
2022-11-18 13:02:22 +0100

    bug hunt

---
+++ BugHunt/Problem1.java
@@ -0,0 +1,24 @@
+package BerlinClock.BugHunt;
+
+public class Problem1 {
+
+
+
+    public class Test {
+
+        private boolean checkDimensions(CLIQUEUnit other, int e) {
+            for(int i = 0, j = 0; i < e; i++, j += 2) {
+                if (dims[i] != other.dims[i]
+                        || bounds[j] != other.bounds[j]
+                        || bounds[j + 1] != bounds[j + 1]) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public static void main(String[] args){
+
+
+        }
+    }
+}
--- FizzBuzz/FizzBuzz.java
+++ FizzBuzz/FizzBuzz.java
@@ -1,9 +1,31 @@
 package BerlinClock.FizzBuzz;
 
+import com.sun.jdi.IntegerValue;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class FizzBuzz {
+    public String Custom(String nums, String names, Integer number) {
+        String[] name = names.split(",");
+        String[] num = nums.split(",");
+
+        String out = "";
+
+        Integer n;
+
+        for(int i=0; i<name.length;i++) {
+            n = Integer.valueOf(num[i]);
+            if (number%n==0){
+                out = out + " " + name[i];
+            }
+        }
+
+        if (out==""){
+            out = Integer.toString(number);
+        }
+        return out;
+    }
 
     public String multipleOf(Integer number) {
         String out = "";
@@ -41,11 +63,11 @@ public class FizzBuzz {
             ar.add("1st test succeeded");
         else
             ar.add("1st test not succeeded");
-        if(multipleOf(2).equals("fuzz"))
+        if(multipleOf(2).equals("2"))
             ar.add("2nd test succeeded");
         else
             ar.add("2nd test not succeeded");
-        if(multipleOf(8).equals("fuzz"))
+        if(multipleOf(8).equals("8"))
             ar.add("3rd test succeeded");
         else
             ar.add("3rd test not succeeded");
@@ -156,15 +178,15 @@ public class FizzBuzz {
     }
     public ArrayList<String>  multiples_of_three_and_five_and_seven_return_fizz_buzz_pop_test(){
         ArrayList<String> ar = new ArrayList<String>();
-        if(multipleOf(105).equals("buzz pop"))
+        if(multipleOf(105).equals("fizz buzz pop"))
             ar.add("1st test succeeded");
         else
             ar.add("1st test not succeeded");
-        if(multipleOf(210).equals("buzz pop"))
+        if(multipleOf(210).equals("fizz buzz pop"))
             ar.add("2nd test succeeded");
         else
             ar.add("2nd test not succeeded");
-        if(multipleOf(315).equals("buzz pop"))
+        if(multipleOf(315).equals("fizz buzz pop"))
             ar.add("3rd test succeeded");
         else
             ar.add("3rd test not succeeded");
@@ -174,48 +196,70 @@ public class FizzBuzz {
 
 
     public static void main(String[] args){
-
         Integer number;
 
         Scanner myObj = new Scanner(System.in);
-        System.out.println("Enter Number");
+        System.out.println("Enter 1 for Custom or 2 for not custom");
         String input = myObj.nextLine();
-        number = Integer.valueOf(input);
-        FizzBuzz fizzBuzz = new FizzBuzz();
+        if (Integer.valueOf(input).equals(2)) {
+            Scanner myObjN = new Scanner(System.in);
+            System.out.println("Enter Number");
+            String inputN = myObjN.nextLine();
+            number = Integer.valueOf(inputN);
+            FizzBuzz fizzBuzz = new FizzBuzz();
+            System.out.println("The 1st test result for normal_numbers_return_same_number_test is : " + fizzBuzz.normal_numbers_return_same_number_test().get(0));
+            System.out.println("The 1st test result for normal_numbers_return_same_number_test is : " + fizzBuzz.normal_numbers_return_same_number_test().get(1));
+            System.out.println("The 1st test result for normal_numbers_return_same_number_test is : " + fizzBuzz.normal_numbers_return_same_number_test().get(2));
+            System.out.println("///////////////////////////////////////");
+            System.out.println("The 1st test result for multiples_of_three_return_fizz_test is : " + fizzBuzz.multiples_of_three_return_fizz_test().get(0));
+            System.out.println("The 1st test result for multiples_of_three_return_fizz_test is : " + fizzBuzz.multiples_of_three_return_fizz_test().get(1));
+            System.out.println("The 1st test result for multiples_of_three_return_fizz_test is : " + fizzBuzz.multiples_of_three_return_fizz_test().get(2));
+            System.out.println("///////////////////////////////////////");
+            System.out.println("The 1st test result for multiples_of_five_return_buzz_test is : " + fizzBuzz.multiples_of_five_return_buzz_test().get(0));
+            System.out.println("The 1st test result for multiples_of_five_return_buzz_test is : " + fizzBuzz.multiples_of_five_return_buzz_test().get(1));
+            System.out.println("The 1st test result for multiples_of_five_return_buzz_test is : " + fizzBuzz.multiples_of_five_return_buzz_test().get(2));
+            System.out.println("///////////////////////////////////////");
+            System.out.println("The 1st test result for multiples_of_three_and_five_return_fizz_buzz_test is : " + fizzBuzz.multiples_of_three_and_five_return_fizz_buzz_test().get(0));
+            System.out.println("The 1st test result for multiples_of_three_and_five_return_fizz_buzz_test is : " + fizzBuzz.multiples_of_three_and_five_return_fizz_buzz_test().get(1));
+            System.out.println("The 1st test result for multiples_of_three_and_five_return_fizz_buzz_test is : " + fizzBuzz.multiples_of_three_and_five_return_fizz_buzz_test().get(2));
+            System.out.println("///////////////////////////////////////");
+            System.out.println("The 1st test result for multiples_of_seven_return_pop_test is : " + fizzBuzz.multiples_of_seven_return_pop_test().get(0));
+            System.out.println("The 1st test result for multiples_of_seven_return_pop_test is : " + fizzBuzz.multiples_of_seven_return_pop_test().get(1));
+            System.out.println("The 1st test result for multiples_of_seven_return_pop_test is : " + fizzBuzz.multiples_of_seven_return_pop_test().get(2));
+            System.out.println("///////////////////////////////////////");
+            System.out.println("The 1st test result for multiples_of_three_and_seven_return_pop_test is : " + fizzBuzz.multiples_of_three_and_seven_return_pop_test().get(0));
+            System.out.println("The 1st test result for multiples_of_three_and_seven_return_pop_test is : " + fizzBuzz.multiples_of_three_and_seven_return_pop_test().get(1));
+            System.out.println("The 1st test result for multiples_of_three_and_seven_return_pop_test is : " + fizzBuzz.multiples_of_three_and_seven_return_pop_test().get(2));
+            System.out.println("///////////////////////////////////////");
+            System.out.println("The 1st test result for multiples_of_five_and_seven_return_buzz_test is : " + fizzBuzz.multiples_of_five_and_seven_return_buzz_test().get(0));
+            System.out.println("The 1st test result for multiples_of_five_and_seven_return_buzz_test is : " + fizzBuzz.multiples_of_five_and_seven_return_buzz_test().get(1));
+            System.out.println("The 1st test result for multiples_of_five_and_seven_return_buzz_test is : " + fizzBuzz.multiples_of_five_and_seven_return_buzz_test().get(2));
+            System.out.println("///////////////////////////////////////");
+            System.out.println("The 1st test result for multiples_of_three_and_five_and_seven_return_fizz_buzz_pop_test is : " + fizzBuzz.multiples_of_three_and_five_and_seven_return_fizz_buzz_pop_test().get(0));
+            System.out.println("The 1st test result for multiples_of_three_and_five_and_seven_return_fizz_buzz_pop_test is : " + fizzBuzz.multiples_of_three_and_five_and_seven_return_fizz_buzz_pop_test().get(1));
+            System.out.println("The 1st test result for multiples_of_three_and_five_and_seven_return_fizz_buzz_pop_test is : " + fizzBuzz.multiples_of_three_and_five_and_seven_return_fizz_buzz_pop_test().get(2));
+            System.out.println("///////////////////////////////////////");
+
+            //System.out.println(fizzBuzz.multipleOfThree(number) + " " + fizzBuzz.multipleOfFive(number));
+            System.out.println(fizzBuzz.multipleOf(number));
+        }
+        else{
+            Scanner myObjC = new Scanner(System.in);
+            System.out.println("Enter Numbers divided by commas that you want to check their multiples");
+            String inputNumbers = myObjC.nextLine();
+            System.out.println("Enter the outputs for each multiple divided by commas");
+            String inputStrings = myObjC.nextLine();
+
+            System.out.println("Enter Number");
+            String inputC = myObjC.nextLine();
+            number = Integer.valueOf(inputC);
+
+            FizzBuzz fizzBuzz = new FizzBuzz();
+
+            System.out.println(fizzBuzz.Custom(inputNumbers,inputStrings, number));
+        }
         //System.out.println(fizzBuzz.multipleOfThree(number) + " " + fizzBuzz.multipleOfFive(number));
-        System.out.println(fizzBuzz.multipleOf(number));
-        System.out.println("The 1st test result for normal_numbers_return_same_number_test is : " + fizzBuzz.normal_numbers_return_same_number_test().get(0));
-        System.out.println("The 1st test result for normal_numbers_return_same_number_test is : " + fizzBuzz.normal_numbers_return_same_number_test().get(1));
-        System.out.println("The 1st test result for normal_numbers_return_same_number_test is : " + fizzBuzz.normal_numbers_return_same_number_test().get(2));
-        System.out.println("///////////////////////////////////////");
-        System.out.println("The 1st test result for multiples_of_three_return_fizz_test is : " + fizzBuzz.multiples_of_three_return_fizz_test().get(0));
-        System.out.println("The 1st test result for multiples_of_three_return_fizz_test is : " + fizzBuzz.multiples_of_three_return_fizz_test().get(1));
-        System.out.println("The 1st test result for multiples_of_three_return_fizz_test is : " + fizzBuzz.multiples_of_three_return_fizz_test().get(2));
-        System.out.println("///////////////////////////////////////");
-        System.out.println("The 1st test result for multiples_of_five_return_buzz_test is : " + fizzBuzz.multiples_of_five_return_buzz_test().get(0));
-        System.out.println("The 1st test result for multiples_of_five_return_buzz_test is : " + fizzBuzz.multiples_of_five_return_buzz_test().get(1));
-        System.out.println("The 1st test result for multiples_of_five_return_buzz_test is : " + fizzBuzz.multiples_of_five_return_buzz_test().get(2));
-        System.out.println("///////////////////////////////////////");
-        System.out.println("The 1st test result for multiples_of_three_and_five_return_fizz_buzz_test is : " + fizzBuzz.multiples_of_three_and_five_return_fizz_buzz_test().get(0));
-        System.out.println("The 1st test result for multiples_of_three_and_five_return_fizz_buzz_test is : " + fizzBuzz.multiples_of_three_and_five_return_fizz_buzz_test().get(1));
-        System.out.println("The 1st test result for multiples_of_three_and_five_return_fizz_buzz_test is : " + fizzBuzz.multiples_of_three_and_five_return_fizz_buzz_test().get(2));
-        System.out.println("///////////////////////////////////////");
-        System.out.println("The 1st test result for multiples_of_seven_return_pop_test is : " + fizzBuzz.multiples_of_seven_return_pop_test().get(0));
-        System.out.println("The 1st test result for multiples_of_seven_return_pop_test is : " + fizzBuzz.multiples_of_seven_return_pop_test().get(1));
-        System.out.println("The 1st test result for multiples_of_seven_return_pop_test is : " + fizzBuzz.multiples_of_seven_return_pop_test().get(2));
-        System.out.println("///////////////////////////////////////");
-        System.out.println("The 1st test result for multiples_of_three_and_seven_return_pop_test is : " + fizzBuzz.multiples_of_three_and_seven_return_pop_test().get(0));
-        System.out.println("The 1st test result for multiples_of_three_and_seven_return_pop_test is : " + fizzBuzz.multiples_of_three_and_seven_return_pop_test().get(1));
-        System.out.println("The 1st test result for multiples_of_three_and_seven_return_pop_test is : " + fizzBuzz.multiples_of_three_and_seven_return_pop_test().get(2));
-        System.out.println("///////////////////////////////////////");
-        System.out.println("The 1st test result for multiples_of_five_and_seven_return_buzz_test is : " + fizzBuzz.multiples_of_five_and_seven_return_buzz_test().get(0));
-        System.out.println("The 1st test result for multiples_of_five_and_seven_return_buzz_test is : " + fizzBuzz.multiples_of_five_and_seven_return_buzz_test().get(1));
-        System.out.println("The 1st test result for multiples_of_five_and_seven_return_buzz_test is : " + fizzBuzz.multiples_of_five_and_seven_return_buzz_test().get(2));
-        System.out.println("///////////////////////////////////////");
-        System.out.println("The 1st test result for multiples_of_three_and_five_and_seven_return_fizz_buzz_pop_test is : " + fizzBuzz.multiples_of_three_and_five_and_seven_return_fizz_buzz_pop_test().get(0));
-        System.out.println("The 1st test result for multiples_of_three_and_five_and_seven_return_fizz_buzz_pop_test is : " + fizzBuzz.multiples_of_three_and_five_and_seven_return_fizz_buzz_pop_test().get(1));
-        System.out.println("The 1st test result for multiples_of_three_and_five_and_seven_return_fizz_buzz_pop_test is : " + fizzBuzz.multiples_of_three_and_five_and_seven_return_fizz_buzz_pop_test().get(2));
-        System.out.println("///////////////////////////////////////");
+
 
 
 
