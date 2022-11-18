f5e4c83276622b6be58cb977ee28d8a6d29b0418
Lungu Marius <Marius.Lungu@solera.com>
2022-11-18 12:18:07 +0100

    berlin clock tests

--- BerlinClock.java
+++ BerlinClock.java
@@ -87,6 +87,153 @@ public class BerlinClock {
 
     }
 
+    public ArrayList<String>  five_hours_row_test(){
+        ArrayList<String> ar = new ArrayList<String>();
+        if(five_hours_row(0).equals("OOOO"))
+            ar.add("1st test succeeded");
+        else
+            ar.add("1st test not succeeded");
+        if(five_hours_row(23).equals("RRRR"))
+            ar.add("2nd test succeeded");
+        else
+            ar.add("2nd test not succeeded");
+        if(five_hours_row(2).equals("OOOO"))
+            ar.add("3rd test succeeded");
+        else
+            ar.add("3rd test not succeeded");
+        if(five_hours_row(8).equals("ROOO"))
+            ar.add("4th test succeeded");
+        else
+            ar.add("4th test not succeeded");
+        if(five_hours_row(16).equals("RRRO"))
+            ar.add("5th test succeeded");
+        else
+            ar.add("5th test not succeeded");
+        return ar;
+    }
+    public ArrayList<String> every_second_test(){
+        ArrayList<String> ar = new ArrayList<String>();
+        if(every_second(00) == "Y")
+            ar.add("1st test succeeded");
+        else
+            ar.add("1st test not succeeded");
+        if(every_second(59) == "O")
+            ar.add("2nd test succeeded");
+        else
+            ar.add("2nd test not succeeded");
+        return ar;
+    }
+    public ArrayList<String> single_hours_row_test(){
+        ArrayList<String> ar = new ArrayList<String>();
+        if(single_hours_row(0).equals("OOOO"))
+            ar.add("1st test succeeded");
+        else
+            ar.add("1st test not succeeded");
+        if(single_hours_row(23).equals("RRRO"))
+            ar.add("2nd test succeeded");
+        else
+            ar.add("2nd test not succeeded");
+        if(single_hours_row(2).equals("RROO"))
+            ar.add("3rd test succeeded");
+        else
+            ar.add("3rd test not succeeded");
+
+        if(single_hours_row(8).equals("RRRO"))
+            ar.add("4th test succeeded");
+        else
+            ar.add("4th test not succeeded");
+        if(single_hours_row(14).equals("RRRR"))
+            ar.add("5th test succeeded");
+        else
+            ar.add("5th test not succeeded");
+
+        return ar;
+    }
+    public ArrayList<String> five_minutes_row_test(){
+        ArrayList<String> ar = new ArrayList<String>();
+        if(five_minutes_row(0).equals("OOOOOOOOOOO"))
+            ar.add("1st test succeeded");
+        else
+            ar.add("1st test not succeeded");
+        if(five_minutes_row(59).equals("YYRYYRYYRYY"))
+            ar.add("2nd test succeeded");
+        else
+            ar.add("2nd test not succeeded");
+        if(five_minutes_row(4).equals("OOOOOOOOOOO"))
+            ar.add("3rd test succeeded");
+        else
+            ar.add("3rd test not succeeded");
+
+        if(five_minutes_row(23).equals("YYRYOOOOOOO"))
+            ar.add("4th test succeeded");
+        else
+            ar.add("4th test not succeeded");
+        if(five_minutes_row(35).equals("YYRYYRYOOOO"))
+            ar.add("5th test succeeded");
+        else
+            ar.add("5th test not succeeded");
+
+        return ar;
+    }
+    public ArrayList<String> single_minutes_row_test(){
+        ArrayList<String> ar = new ArrayList<String>();
+        if(single_minutes_row(0).equals("OOOO"))
+            ar.add("1st test succeeded");
+        else
+            ar.add("1st test not succeeded");
+        if(single_minutes_row(59).equals("YYYY"))
+            ar.add("2nd test succeeded");
+        else
+            ar.add("2nd test not succeeded");
+        if(single_minutes_row(32).equals("YYOO"))
+            ar.add("3rd test succeeded");
+        else
+            ar.add("3rd test not succeeded");
+
+        if(single_minutes_row(34).equals("YYYY"))
+            ar.add("4th test succeeded");
+        else
+            ar.add("4th test not succeeded");
+        if(single_minutes_row(35).equals("OOOO"))
+            ar.add("5th test succeeded");
+        else
+            ar.add("5th test not succeeded");
+
+        return ar;
+    }
+    public ArrayList<String> entire_berlin_clock_test(){
+        ArrayList<String> ar = new ArrayList<String>();
+        if((every_second(0) + five_hours_row(0)
+                + single_hours_row(0) + five_minutes_row(0)
+                + single_minutes_row(0)).equals("YOOOOOOOOOOOOOOOOOOOOOOO"))
+            ar.add("1st test succeeded");
+        else
+            ar.add("1st test not succeeded");
+        if((every_second(59) + five_hours_row(23)
+                + single_hours_row(23) + five_minutes_row(59)
+                + single_minutes_row(59)).equals("ORRRRRRROYYRYYRYYRYYYYYY"))
+            ar.add("2nd test succeeded");
+        else
+            ar.add("2nd test not succeeded");
+        if((every_second(6) + five_hours_row(16)
+                + single_hours_row(16) + five_minutes_row(50)
+                + single_minutes_row(50)).equals("YRRROROOOYYRYYRYYRYOOOOO"))
+            ar.add("3rd test succeeded");
+        else
+            ar.add("3rd test not succeeded");
+
+        if((every_second(1) + five_hours_row(11)
+                + single_hours_row(11) + five_minutes_row(37)
+                + single_minutes_row(37)).equals("ORROOROOOYYRYYRYOOOOYYOO"))
+            ar.add("4th test succeeded");
+        else
+            ar.add("4th test not succeeded");
+
+
+        return ar;
+    }
+
+
 
 
 
@@ -108,6 +255,42 @@ public class BerlinClock {
         minutes = Integer.valueOf(newTime[1]);
         seconds = Integer.valueOf(newTime[2]);
         BerlinClock berlinClock = new BerlinClock();
+
+
+        System.out.println("1st Test for Every Second is :  " + berlinClock.every_second_test().get(0));
+        System.out.println("2nd Test for Every Second is :  " + berlinClock.every_second_test().get(1));
+        System.out.println("////////////////");
+        System.out.println("1st Test for Five Hours Row is :  " + berlinClock.five_hours_row_test().get(0));
+        System.out.println("2nd Test for Five Hours Row is :  " + berlinClock.five_hours_row_test().get(1));
+        System.out.println("3rd Test for Five Hours Row is :  " + berlinClock.five_hours_row_test().get(2));
+        System.out.println("4th Test for Five Hours Row is :  " + berlinClock.five_hours_row_test().get(3));
+        System.out.println("5th Test for Five Hours Row is :  " + berlinClock.five_hours_row_test().get(4));
+        System.out.println("////////////////");
+        System.out.println("1st Test for Single Hours Row is :  " + berlinClock.single_hours_row_test().get(0));
+        System.out.println("2nd Test for Single Hours Row is :  " + berlinClock.single_hours_row_test().get(1));
+        System.out.println("3rd Test for Single Hours Row is :  " + berlinClock.single_hours_row_test().get(2));
+        System.out.println("4th Test for Single Hours Row is :  " + berlinClock.single_hours_row_test().get(3));
+        System.out.println("5th Test for Single Hours Row is :  " + berlinClock.single_hours_row_test().get(4));
+        System.out.println("////////////////");
+        System.out.println("1st Test for Five Minutes Row is :  " + berlinClock.five_minutes_row_test().get(0));
+        System.out.println("2nd Test for Five Minutes Row is :  " + berlinClock.five_minutes_row_test().get(1));
+        System.out.println("3rd Test for Five Minutes Row is :  " + berlinClock.five_minutes_row_test().get(2));
+        System.out.println("4th Test for Five Minutes Row is :  " + berlinClock.five_minutes_row_test().get(3));
+        System.out.println("5th Test for Five Minutes Row is :  " + berlinClock.five_minutes_row_test().get(4));
+        System.out.println("////////////////");
+        System.out.println("1st Test for Single Minutes Row is :  " + berlinClock.five_minutes_row_test().get(0));
+        System.out.println("2nd Test for Single Minutes Row is :  " + berlinClock.five_minutes_row_test().get(1));
+        System.out.println("3rd Test for Single Minutes Row is :  " + berlinClock.five_minutes_row_test().get(2));
+        System.out.println("4th Test for Single Minutes Row is :  " + berlinClock.five_minutes_row_test().get(3));
+        System.out.println("5th Test for Single Minutes Row is :  " + berlinClock.five_minutes_row_test().get(4));
+        System.out.println("////////////////");
+        System.out.println("1st Test for Entire Berlin Clock is  :  " + berlinClock.entire_berlin_clock_test().get(0));
+        System.out.println("2nd Test for Entire Berlin Clock is  :  " + berlinClock.entire_berlin_clock_test().get(1));
+        System.out.println("3rd Test for Entire Berlin Clock is  :  " + berlinClock.entire_berlin_clock_test().get(2));
+        System.out.println("4th Test for Entire Berlin Clock is  :  " + berlinClock.entire_berlin_clock_test().get(3));
+
+
+
         String entireBerlinClock = berlinClock.every_second(seconds) + berlinClock.five_hours_row(hours)
                 + berlinClock.single_hours_row(hours) + berlinClock.five_minutes_row(minutes) + berlinClock.single_minutes_row(minutes);
         System.out.println(entireBerlinClock);
