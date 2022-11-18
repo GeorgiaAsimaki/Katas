4217d96c309dd89519ec4c0a08b374636f0d5fc5
Lungu Marius <Marius.Lungu@solera.com>
2022-11-18 13:03:04 +0100

    bug hunt first problem solved

--- BugHunt/Problem1.java
+++ BugHunt/Problem1.java
@@ -10,7 +10,7 @@ public class Problem1 {
             for(int i = 0, j = 0; i < e; i++, j += 2) {
                 if (dims[i] != other.dims[i]
                         || bounds[j] != other.bounds[j]
-                        || bounds[j + 1] != bounds[j + 1]) {
+                        || bounds[j] != bounds[j + 1]) {
                     return false;
                 }
             }
