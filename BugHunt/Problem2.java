afed12fb889f25846e13915b23d1d29d691ac6d1
Lungu Marius <Marius.Lungu@solera.com>
2022-11-18 16:45:07 +0100

    2nd problem solution

--- BugHunt/Problem2.java
+++ BugHunt/Problem2.java
@@ -1,9 +1,10 @@
 package BerlinClock.BugHunt;
 
 public class Problem2 {
-    // 2nd problem -
+    // 2nd problem - ternar operation asking if it s exactly one element in the array tho
+    // getting the second, we need to get the first element
 
 //        public double[] computeMean(){
-//            return points.size() == 1 ? points.get(1) : null;
+//            return points.size() == 1 ? points.get(0) : null;
 //        }
 }
