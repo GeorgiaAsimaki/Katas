259732ac07296d9a419026fae2933213f019bb1b
Lungu Marius <Marius.Lungu@solera.com>
2022-11-18 16:43:51 +0100

    1st problem solution 2nd problem written

--- BugHunt/Problem1.java
+++ BugHunt/Problem1.java
@@ -1,25 +1,28 @@
 package BerlinClock.BugHunt;
-/*
+
 public class Problem1 {
 
 
 
     public class Test {
 
-        private boolean checkDimensions(CLIQUEUnit other, int e) {
-            for(int i = 0, j = 0; i < e; i++, j += 2) {
-                if (dims[i] != other.dims[i]
-                        || bounds[j] != other.bounds[j]
-                        || bounds[j] != bounds[j + 1]) {
-                    return false;
-                }
-            }
-            return true;
-        }
+       // 1st problem - the condition will always be false, we must compare as above
+
+//        private boolean checkDimensions(CLIQUEUnit other, int e) {
+//            for(int i = 0, j = 0; i < e; i++, j += 2) {
+//                if (dims[i] != other.dims[i]
+//                        || bounds[j] != other.bounds[j]
+//                        || bounds[j] != other.bounds[j + 1]) {
+//                    return false;
+//                }
+//            }
+//            return true;
+//        }
+
+
         public static void main(String[] args){
 
 
         }
     }
 }
-*/
---
+++ BugHunt/Problem2.java
@@ -0,0 +1,9 @@
+package BerlinClock.BugHunt;
+
+public class Problem2 {
+    // 2nd problem -
+
+//        public double[] computeMean(){
+//            return points.size() == 1 ? points.get(1) : null;
+//        }
+}
